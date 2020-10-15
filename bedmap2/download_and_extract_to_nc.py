0f5b09592e050d8e29e373260608367e9536d2d3
m-kreuzer <kreuzer@pik-potsdam.de>
2020-10-15 16:49:41 +0200

    added references to bedmap2, racmo_wessem, tillphi_pism

--- bedmap2/download_and_extract_to_nc.py
+++ bedmap2/download_and_extract_to_nc.py
@@ -18,6 +18,7 @@ import pism_input.pism_input as pi; reload(pi)
 ### Bedmap2   ##########################################################
 # Documentation of the data: https://www.bas.ac.uk/project/bedmap-2/
 bedmap2_link="https://secure.antarctica.ac.uk/data/bedmap2/bedmap2_bin.zip"
+reference_short = "Fretwell, P. et al: Bedmap2: improved ice bed, surface and thickness datasets for Antarctica, The Cryosphere, 7, 375-393, doi:10.5194/tc-7-375-2013, 2013."
 ## such file definitions should go to config.py, so that other functions can access them.
 bedmap2_data_path = os.path.join(cf.output_data_path, "bedmap2")
 ncout_name = os.path.join(bedmap2_data_path, 'bedmap2_1km_input.nc')
@@ -66,19 +67,19 @@ for var, file in data_files.iteritems():
 
 bedm2_attributes = {"topg": {"long_name" : "elevation of bedrock",
                            "standard_name" : "bedrock_altitude",
-                           "units" : "meters"},
+                           "units" : "meters", "reference" : reference_short },
                     "usurf": {"long_name" : "ice upper surface elevation",
                             "standard_name" : "surface_altitude",
-                            "units" : "meters"},
+                            "units" : "meters", "reference" : reference_short },
                     "thk": {"long_name" : "thickness of ice sheet or ice shelf",
                             "standard_name" : "land_ice_thickness",
-                            "units" : "meters"},
+                            "units" : "meters", "reference" : reference_short },
                     "bedunc": {"long_name" : "uncertainty of bed topography",
                             "standard_name" : "bed_uncertainty",
-                            "units" : "meters"},
+                            "units" : "meters", "reference" : reference_short},
                     "mask": {"long_name" : "ice-type (ice-free/grounded/floating/ocean) integer mask",
                             "standard_name" : "mask",
-                            "units" : ""} }
+                            "units" : "", "reference" : reference_short} }
 
 print "Writing NetCDF file '%s' ..." % ncout_name
 ncout = netCDF4.Dataset(ncout_name,"w",format='NETCDF4_CLASSIC')
@@ -103,6 +104,7 @@ now = datetime.datetime.now().strftime("%B %d, %Y")
 ncout.proj4 = cf.proj4str
 ncout.comment  = cf.authors+" created netcdf bedmap2 file at " + now
 ncout.reference = "Fretwell, P., Pritchard, H. D., Vaughan, D. G., Bamber, J. L., Barrand, N. E., Bell, R., Bianchi, C., Bingham, R. G., Blankenship, D. D., Casassa, G., Catania, G., Callens, D., Conway, H., Cook, A. J., Corr, H. F. J., Damaske, D., Damm, V., Ferraccioli, F., Forsberg, R., Fujita, S., Gim, Y., Gogineni, P., Griggs, J. A., Hindmarsh, R. C. A., Holmlund, P., Holt, J. W., Jacobel, R. W., Jenkins, A., Jokat, W., Jordan, T., King, E. C., Kohler, J., Krabill, W., Riger-Kusk, M., Langley, K. A., Leitchenkov, G., Leuschen, C., Luyendyk, B. P., Matsuoka, K., Mouginot, J., Nitsche, F. O., Nogi, Y., Nost, O. A., Popov, S. V., Rignot, E., Rippin, D. M., Rivera, A., Roberts, J., Ross, N., Siegert, M. J., Smith, A. M., Steinhage, D., Studinger, M., Sun, B., Tinto, B. K., Welch, B. C., Wilson, D., Young, D. A., Xiangbin, C., and Zirizzotti, A.: Bedmap2: improved ice bed, surface and thickness datasets for Antarctica, The Cryosphere, 7, 375-393, doi:10.5194/tc-7-375-2013, 2013."
+ncout.source = bedmap2_link
 ncout.documentation = "https://www.bas.ac.uk/project/bedmap-2/"
 ncout.close()
 
