f0fae8d0a84943b53f4a885ab6bbbd2bda77fef0
Julius Garbe <julius.garbe@pik-potsdam.de>
2021-05-03 19:18:08 +0200

    Make RACMO data ready for PISM-dEBM

--- racmo_cesm2/cdo_remap_hist.sh
+++ racmo_cesm2/cdo_remap_hist.sh
@@ -26,6 +26,8 @@ module load nco
 export REMAP_EXTRAPOLATE=on
 
 grid='initmip8km'
+#grid='initmip16km'
+#grid='initmip32km'
 
 datadir=/p/projects/pism/garbe/2018_PISM_Input_Data
 
@@ -33,11 +35,14 @@ griddatadir=$datadir/cdo_remapgrids
 inputdatadir=$datadir/racmo_cesm2/input
 outputdatadir=$datadir/racmo_cesm2/$grid
 
+inputfile=racmo_cesm2_hist_input.nc
+outputfile=racmo_cesm2_hist_1950-2014_monthly_$grid.nc
+
 # bilinear interpolation, an option if conservative methods fail.
-$cdocmd -b F64 -f nc4c remapbil,$griddatadir/$grid.nc $inputdatadir/racmo_cesm2_hist.nc $outputdatadir/racmo_cesm2_hist_$grid.nc
+$cdocmd -b F64 -f nc4c remapbil,$griddatadir/$grid.nc $inputdatadir/$inputfile $outputdatadir/$outputfile
 
 
 # add x and y variables to output file.
-ncks -A -v x,y $griddatadir/$grid.nc $outputdatadir/racmo_cesm2_hist_$grid.nc
+ncks -A -v x,y $griddatadir/$grid.nc $outputdatadir/$outputfile
 
-echo "regridded file is $outputdatadir/racmo_cesm2_hist_$grid.nc"
+echo "regridded file is $outputdatadir/$outputfile"
--- racmo_cesm2/merge_and_rename_variables_hist.py
+++ racmo_cesm2/merge_and_rename_variables_hist.py
@@ -15,19 +15,31 @@ import config as cf; reload(cf)
 import pism_input.pism_input as pi; reload(pi)
 
 dataset = "racmo_cesm2"
-data_path = os.path.join(cf.output_data_path, dataset)
+data_path = os.path.join(cf.output_data_path, dataset, "input")
 
 if not os.path.exists(data_path): os.makedirs(data_path)
 
-output_file = os.path.join(data_path, dataset+"_input_hist.nc")
+output_file = os.path.join(data_path, dataset+"_hist_input.nc")
 
-source_file = {"tskin": os.path.join(cf.racmo_cesm2_data_path,"hist/tskin_monthlyA_ANT27_CESM2_RACMO2.3p2_195001_201412.nc"),
-               "smb": os.path.join(cf.racmo_cesm2_data_path,"hist/smb_monthlyS_ANT27_CESM2_RACMO2.3p2_195001_201412.nc")}
-               #"t2m": os.path.join(cf.racmo_cesm2_data_path,"hist/t2m_monthlyA_ANT27_CESM2_RACMO2.3p2_195001_201412.nc"),
-               #"precip": os.path.join(cf.racmo_cesm2_data_path,"hist/precip_monthlyS_ANT27_CESM2_RACMO2.3p2_195001_201412.nc"),
-               #}
+source_file = {"alb": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/alb_monthlyA_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "snowmelt": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/snowmelt_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               #"tskin": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/tskin_monthlyA_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "t2m": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/t2m_monthlyA_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "smb": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/smb_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "refreeze": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/refreeze_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "runoff": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/runoff_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "precip": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/precip_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               #"evap": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/evap_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_r567_195001_201412.nc"),
+               #"subl": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/subl_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "sw0d": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/sw0d_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               "swsd": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/swsd_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               #"lwsd": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/lwsd_monthlyS_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"), # needed to compute atmospheric emissivity
+               #"clcov": os.path.join(cf.racmo_cesm2_data_path,"hist_r567/clcov_monthlyA_ANT27_CESM2_RACMO2.3p2_hist_r567_195001_201412.nc"),
+               }
 
-process_file = {var:os.path.join(data_path, dataset+"_"+var+"_hist.nc") for var in ["tskin","smb"]}
+var_list = ["alb","snowmelt","t2m","smb","refreeze","runoff","precip","sw0d","swsd"]
+
+process_file = {var:os.path.join(data_path, dataset+"_hist_"+var+".nc") for var in var_list}
 
 for var,fl in process_file.iteritems():
     try:
@@ -35,8 +47,8 @@ for var,fl in process_file.iteritems():
     except OSError:
         pass
 
-#for var in ["tskin","smb","t2m","precip"]:
-for var in ["tskin","smb"]:
+for var in var_list:
+    print var
 
     subprocess.check_call("ncks -A -v "+var+",lon,lat "+source_file[var]+" "+process_file[var],shell=True)
 
@@ -74,14 +86,15 @@ for var in ["tskin","smb"]:
 
 # process_file["smb"] = smb_omask_file
 
-#merge_these_files = " ".join([process_file[var] for var in ["tskin","smb","t2m","precip"]])
-merge_these_files = " ".join([process_file[var] for var in ["tskin","smb"]])
+print "Merging files..."
+
+merge_these_files = " ".join([process_file[var] for var in var_list])
 
 subprocess.check_call('cdo -O merge '+merge_these_files+" "+output_file, shell=True)
 
 # make all variables double (some already are).
-subprocess.check_call("ncap2 -O -s 'tskin=double(tskin);smb=double(smb);' "+
-                      output_file+" "+output_file,shell=True)
+ncap2str = ''.join([var+"=double("+var+");" for var in var_list]) # join commands for all variables in var_list into common string
+subprocess.check_call("ncap2 -O -s '"+ncap2str+"' "+output_file+" "+output_file,shell=True)
 
 # Fill the missing SMB field over ocean with the proxy precip - evaporation
 #ncf = nc.Dataset(output_file,"a")
@@ -96,26 +109,65 @@ subprocess.check_call("ncap2 -O -s 'tskin=double(tskin);smb=double(smb);' "+
 #ncf.smb_comment = "SMB is approximated by precip-evap over the ocean."
 #ncf.close()
 
-# attribute fixes
+# convert units of solar flux densities from J m-2 to W m-2 (RACMO sums over a month). Not exactly accurate, as months have different lengths.
+subprocess.check_call("ncap2 -O -s 'sw0d=sw0d/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncap2 -O -s 'swsd=swsd/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+#subprocess.check_call("ncap2 -O -s 'lwsd=lwsd/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+
+subprocess.check_call('ncatted -a units,sw0d,o,c,"W m-2" '+output_file,shell=True)
+subprocess.check_call('ncatted -a units,swsd,o,c,"W m-2" '+output_file,shell=True)
+#subprocess.check_call('ncatted -a units,lwsd,o,c,"W m-2" '+output_file,shell=True)
+
+# convert units of SMB fluxes from kg m-2 to kg m-2 s-1 (RACMO sums over a month). Not exactly accurate, as months have different lengths.
+subprocess.check_call("ncap2 -O -s 'snowmelt=snowmelt/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncap2 -O -s 'smb=smb/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncap2 -O -s 'refreeze=refreeze/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncap2 -O -s 'runoff=runoff/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncap2 -O -s 'precip=precip/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+#subprocess.check_call("ncap2 -O -s 'evap=evap/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+#subprocess.check_call("ncap2 -O -s 'subl=subl/(60.*60.*24.*30.4167)' "+output_file+" "+output_file,shell=True)
+
+subprocess.check_call('ncatted -a units,snowmelt,o,c,"kg m-2 s-1" '+output_file,shell=True)
+subprocess.check_call('ncatted -a units,smb,o,c,"kg m-2 s-1" '+output_file,shell=True)
+subprocess.check_call('ncatted -a units,refreeze,o,c,"kg m-2 s-1" '+output_file,shell=True)
+subprocess.check_call('ncatted -a units,runoff,o,c,"kg m-2 s-1" '+output_file,shell=True)
+subprocess.check_call('ncatted -a units,precip,o,c,"kg m-2 s-1" '+output_file,shell=True)
+#subprocess.check_call('ncatted -a units,evap,o,c,"kg m-2 s-1" '+output_file,shell=True)
+#subprocess.check_call('ncatted -a units,subl,o,c,"kg m-2 s-1" '+output_file,shell=True)
+
+# further attribute fixes
 subprocess.check_call('ncatted -a units,x,o,c,"meters" '+output_file,shell=True)
 subprocess.check_call('ncatted -a units,y,o,c,"meters" '+output_file,shell=True)
-#subprocess.check_call('ncatted -a units,time_bnds,o,c,"days since 1950-01-01 00:00:00.0" '+output_file,shell=True)
 
-subprocess.check_call('ncatted -a units,smb,o,c,"kg m-2 year-1" '+output_file,shell=True)
-#subprocess.check_call('ncatted -a units,precip,o,c,"kg m-2 year-1" '+output_file,shell=True)
+subprocess.check_call('ncatted -a units,time_bnds,o,c,"days since 1950-01-01 00:00:00.0" '+output_file,shell=True)
+subprocess.check_call('ncatted -a coordinates,time_bnds,d,, '+output_file,shell=True)
+
+subprocess.check_call('ncatted -a units,alb,o,c,"-" '+output_file,shell=True)
 
 # rename variables
-subprocess.check_call("ncrename -v tskin,ice_surface_temp -O "+output_file+" "+output_file,shell=True)
-subprocess.check_call("ncrename -v smb,climatic_mass_balance -O "+output_file+" "+output_file,shell=True)
-#subprocess.check_call("ncrename -v t2m,air_temp -O "+output_file+" "+output_file,shell=True)
-#subprocess.check_call("ncrename -v precip,precipitation -O "+output_file+" "+output_file,shell=True)
-
+subprocess.check_call("ncrename -v .alb,albedo -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .tskin,ice_surface_temp -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .t2m,air_temp -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .smb,climatic_mass_balance -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .precip,precipitation -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .evap,evaporation -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .subl,sublimation -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .sw0d,incoming_shortwave_radiation_TOA -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .swsd,incoming_shortwave_radiation -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .lwsd,incoming_longwave_radiation -O "+output_file+" "+output_file,shell=True)
+subprocess.check_call("ncrename -v .clcov,cloud_cover -O "+output_file+" "+output_file,shell=True)
 
 subprocess.check_call('ncks -O -C -x -v lon_2,lat_2,lon_3,lat_3 '+output_file+" "+output_file, shell=True)
 #RACMO grid actually comes on a lon lat coordinate, so multipliaction here provides values close to meters, but this is not important here
 subprocess.check_call("ncap2 -O -s 'x=x*1.0e5;y=y*1.0e5' "+output_file+" "+output_file,shell=True)
 #subprocess.check_call("ncks -C -O -x -v lon,lat "+output_file+" "+output_file,shell=True)
 
+## since RACMO doesn't include atmospheric emissivity, we compute it according to the Stefan-Boltzmann law from longwave downward surface radiation and 2m air temperature
+#subprocess.check_call("ncap2 -O -s 'emissivity=incoming_longwave_radiation/(5.67e-8 * air_temp^4)' "+output_file+" "+output_file,shell=True)
+#subprocess.check_call('ncatted -a long_name,emissivity,o,c,"Atmospheric Emissivity" '+output_file,shell=True)
+#subprocess.check_call('ncatted -a standard_name,emissivity,o,c,"atmospheric_emissivity" '+output_file,shell=True)
+#subprocess.check_call('ncatted -a units,emissivity,o,c,"-" '+output_file,shell=True)
+#subprocess.check_call("ncks -O -x -v incoming_longwave_radiation "+output_file+" "+output_file,shell=True) # delete, since it is not needed anymore
 
 # prepare the input file for cdo remapping
 # this step takes a while for high resolution data (i.e. 1km)
