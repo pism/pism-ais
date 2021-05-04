7e26a0ceff41317d1fc00013b19d8f47e069133a
Julius Garbe <julius.garbe@pik-potsdam.de>
2021-05-04 13:24:17 +0200

    racmo_cesm2: Improve fill_missing_data script

--- racmo_cesm2/fill_missing_data.py
+++ racmo_cesm2/fill_missing_data.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python
 
 """
-In case of gaps in RACMO data (missing months), we can apply a workaround and replace the missing month by interpolating between the preceding and subsequent months.
+In case of gaps in the RACMO data (missing timeslices in some variables), we can apply a workaround 
+and fill the missing data by interpolating between the preceding and subsequent timeslices.
 
-julius.garbe@pik, 2021
+julius.garbe@pik
 """
 
-import shutil as su
+import os, sys
+import shutil
 import netCDF4 as nc
+## this hack is needed to import config.py from the project root
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path: sys.path.append(project_root)
+import config as cf; reload(cf)
+
+dataset = "racmo_cesm2"
+period = "hist" # historical: "hist" / SSP5-8.5: "ssp585"
+period_years = "1950_2014" # hist: "1950_2014" / ssp585: "2015_2099"
+timestep = "monthly" # time step of input data ("monthly" or "yearly")
+grid = cf.grid_id
+
+data_path = os.path.join(cf.output_data_path, dataset)
+inputfile = os.path.join(data_path, grid, dataset+"_"+period+"_"+period_years+"_"+timestep+"_"+grid+".nc")
+outputfile = inputfile.replace('.nc','_patched.nc')   # this will be the patched file
+
+i_missing = [65,]   # indices of missing timeslices
+
+varnames = ['albedo','air_temp','snowmelt']    # variables showing the data gaps
 
 #############################
 
-file_path = '/p/projects/pism/garbe/2018_PISM_Input_Data/racmo_cesm2/initmip8km/'
-file_in = 'racmo_cesm2_hist_1950_2014_monthly_initmip8km.nc'
-file_out = file_in.replace('.nc','_patched.nc')   # this will be the patched file
-
-i_missing = [65,]   # indices of missing months
-
-varnames = ['albedo','air_temp','ice_surface_temp','snowmelt']
-
-#############################
-
-# Duplicate original file first to avoid overwriting it (depending on file size, this step might take a while)
-su.copyfile(file_path+file_in, file_path+file_out)
+# Duplicate original file first to avoid overwriting it
+if not os.path.exists(outputfile):
+    print "Copy original file... (depending on file size, this might take a while)"
+    shutil.copyfile(inputfile, outputfile)
 
 # Read in the data
-data = nc.Dataset(file_path+file_out, mode="r+", format="NETCDF4_CLASSIC")
+data = nc.Dataset(outputfile, mode="r+", format="NETCDF4_CLASSIC")
 
 vardata = {}
 
+print "Patch variables..."
+
 for var in varnames:
+    print var
 
     vardata[var] = data.variables[var]
 
@@ -38,3 +53,4 @@ for var in varnames:
 
 data.close()
 
+print "Done."
