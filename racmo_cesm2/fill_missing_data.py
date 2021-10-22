c82f87e481c84736908835be0726df8b151a0fee
Julius Garbe <julius.garbe@pik-potsdam.de>
2021-10-22 13:57:51 +0200

    racmo_cesm2: Add ability to handle back-to-back data gaps in fill_missing_data.py

--- racmo_cesm2/fill_missing_data.py
+++ racmo_cesm2/fill_missing_data.py
@@ -16,8 +16,8 @@ if project_root not in sys.path: sys.path.append(project_root)
 import config as cf; reload(cf)
 
 dataset = "racmo_cesm2"
-period = "hist" # historical: "hist" / SSP5-8.5: "ssp585"
-period_years = "1950_2014" # hist: "1950_2014" / ssp585: "2015_2099"
+period = "ssp585" # historical: "hist" / SSP5-8.5: "ssp585"
+period_years = "2015_2099" # hist: "1950_2014" / ssp585: "2015_2099"
 timestep = "monthly" # time step of input data ("monthly" or "yearly")
 grid = cf.grid_id
 
@@ -25,9 +25,11 @@ data_path = os.path.join(cf.output_data_path, dataset)
 inputfile = os.path.join(data_path, grid, dataset+"_"+period+"_"+period_years+"_"+timestep+"_"+grid+".nc")
 outputfile = inputfile.replace('.nc','_patched.nc')   # this will be the patched file
 
-i_missing = [65,]   # indices of missing timeslices
+#i_missing = [65,]   # hist # indices of missing timeslices (first timeslice = 0)
+i_missing = [432,433,434]   # ssp585 # indices of missing timeslices (first timeslice = 0)
 
-varnames = ['albedo','air_temp','snowmelt']    # variables showing the data gaps
+#varnames = ['albedo','air_temp','snowmelt']    # hist # variables showing the data gaps
+varnames = ['albedo','air_temp']    #ssp585 # variables showing the data gaps
 
 #############################
 
@@ -48,8 +50,17 @@ for var in varnames:
 
     vardata[var] = data.variables[var]
 
-    for i in i_missing:
-        vardata[var][i] = (vardata[var][i-1] + vardata[var][i+1]) / 2.
+    # # if missing timeslices all occur isolated between available ones
+    # for i in i_missing:
+    #     vardata[var][i] = (vardata[var][i-1] + vardata[var][i+1]) / 2.
+
+    # if missing timeslices occur back-to-back
+    i_prev = i_missing[0]-1 # last available timeslice
+    i_next = i_missing[-1]+1 # next available timeslice
+
+    for k,i in enumerate(i_missing):
+        factor = (k+1.) / (len(i_missing)+1.)
+        vardata[var][i] = vardata[var][i_prev] + factor * (vardata[var][i_next] - vardata[var][i_prev])
 
 data.close()
 
